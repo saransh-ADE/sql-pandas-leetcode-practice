fa58fe85d4e687f9090b3efc0ebea995f5f30c84
saransh-ADE <saranshazure2026@gmail.com>
2026-04-05 22:47:00 +0530

    Create solution.py

---
+++ SQL_50/1068_Product_Sales_Analysis_I/solution.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
+    result = (
+        sales.merge(product,on='product_id',how='inner')
+        [['product_name','year','price']]
+    )
+
+    return result
