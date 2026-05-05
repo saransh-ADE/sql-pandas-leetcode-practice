b6ad4ffbd19f4586a1cfeff48794c63befa14fc4
saransh-ADE <saranshazure2026@gmail.com>
2026-05-06 00:12:11 +0530

    Update 1164. Product Price at a Given Date.sql

--- SQL_50/1164_Product_Price_at_a_Given_Date/1164. Product Price at a Given Date.sql
+++ SQL_50/1164_Product_Price_at_a_Given_Date/1164. Product Price at a Given Date.sql
@@ -1,1 +1,16 @@
-
+WITH cte
+AS (
+	SELECT product_id
+		,new_price
+		,change_date
+		,dense_rank() OVER (
+			PARTITION BY product_id ORDER BY change_date DESC
+			) AS rn
+	FROM products
+	WHERE change_date <= '2019-08-16'
+	)
+SELECT DISTINCT p.product_id
+	,coalesce(c.new_price, 10) AS price
+FROM products p
+LEFT JOIN cte c ON p.product_id = c.product_id
+	AND rn = 1
